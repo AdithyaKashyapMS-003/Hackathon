f57beb99730688c98f65269cf6e3e95aa8e03a45
prashu26 <prashugowda179@gmail.com>
2025-08-23 00:32:18 +0530

    Resolved merge conflict in package.json

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,44 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import  mongoose  from 'mongoose';
+import dotenv from 'dotenv';
 
+import cors from 'cors';
+import authRouter from './routes/auth.route.js';
+
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
+
 const app = express();
+
 app.use(cors());
+
 app.use(express.json());
 
-// Simple route
-app.get("/", (req, res) => {
-  res.send("Hello from backend 🚀");
+app.use(cookieParser());
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("Connected to MongoDB")
+}).catch((err)=>{
+    console.log(err);
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running in the port 3000")
+})
+
+
+
+
+app.use('/api/auth',authRouter);
+
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internet Server Error';
+    return res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    });
 });
-
-// MongoDB connect
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.error("❌ MongoDB error:", err));
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
