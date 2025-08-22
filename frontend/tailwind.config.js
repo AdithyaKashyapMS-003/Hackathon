efac34df9cf70845efd0e7ffa297d6ff0eba8ade
AdithyaKashyapMS-003 <adithya_23cseu003@uvce.ac.in>
2025-08-22 23:27:19 +0530

    frontend- home screen design

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
