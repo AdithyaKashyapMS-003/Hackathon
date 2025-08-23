1d44bfbaa7a83349d4ae5cadc635ad5d0c825a6b
Adithya Kashyap M S <adithya_23cseu003@uvce.ac.in>
2025-08-23 16:00:54 +0530

    Rename Signin component to SignIn

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-function Signin() {
+function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
@@ -93,4 +93,4 @@ function Signin() {
   );
 }
 
-export default Signin;
+export default SignIn;
